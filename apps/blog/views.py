7491b1e05c03033ced52c2a45be13e78362d5b83
Rotari Artiom <ordersone@gmail.com>
2020-06-14 19:22:57 +0300

    3. Make an endpoint for create a blog post (similar as register endpoint) that will add a new record in blog table

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework.generics import GenericAPIView, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from drf_util.decorators import serialize_decorator
 
 from apps.blog.models import Category, Blog
 from apps.blog.serializers import CategorySerializer, BlogSerializer
@@ -22,6 +23,21 @@ class BlogListView(GenericAPIView):
         blogs = Blog.objects.all()
 
         return Response(BlogSerializer(blogs, many=True).data)
+
+    @serialize_decorator(BlogSerializer)
+    def post(self, request):
+        validated_data = request.serializer.validated_data
+
+        blog = Blog.objects.create(
+            title=validated_data['title'],
+            slug=validated_data['slug'],
+            body=validated_data['body'],
+            category=validated_data['category'],
+            enabled=validated_data['enabled']
+        )
+        blog.save()
+
+        return Response(BlogSerializer(blog).data)
 
 
 class BlogItemView(GenericAPIView):
